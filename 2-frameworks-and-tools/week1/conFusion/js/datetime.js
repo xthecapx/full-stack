75ee46a94c7c88b9b29a066541989b3792de31e4
xthecapx <xthecapx@gmail.com>
2016-04-28 19:29:38 -0500

    Exercise 4.1 added

--- 2-frameworks-and-tools/week1/conFusion/js/datetime.js
+++ 2-frameworks-and-tools/week1/conFusion/js/datetime.js
@@ -11,4 +11,13 @@ $(function () {
 
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();
+
+  $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carousel-pause").click(function() {
+    $("#mycarousel").carousel('pause');
+  });
+
+  $("#carousel-play").click(function() {
+    $("#mycarousel").carousel('cycle');
+  });
 });
