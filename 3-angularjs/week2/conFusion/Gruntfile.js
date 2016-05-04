c2dbbdb36e5b43fc2cc71cdb437d28e1377cd7cd
xthecapx <xthecapx@gmail.com>
2016-05-03 21:11:38 -0500

    Exercise 3.2.1: Watch task added, all ok

--- 3-angularjs/week2/conFusion/Gruntfile.js
+++ 3-angularjs/week2/conFusion/Gruntfile.js
@@ -111,6 +111,51 @@ module.exports = function (grunt) {
       options: {
         assetsDirs: ['dist', 'dist/styles']
       }
+    },
+
+    watch: {
+      copy: {
+        files: [ 'app/**', '!app/**/*.css', '!app/**/*.js'],
+        tasks: [ 'build' ]
+      },
+      scripts: {
+        files: ['app/scripts/app.js'],
+        tasks:[ 'build']
+      },
+      styles: {
+        files: ['app/styles/mystyles.css'],
+        tasks:['build']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          'app/{,*/}*.html',
+          '.tmp/styles/{,*/}*.css',
+          'app/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+    connect: {
+      options: {
+        port: 9000,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost',
+        livereload: 35729
+      },
+      dist: {
+        options: {
+          open: true,
+          base:{
+            path: 'dist',
+            options: {
+              index: 'menu.html',
+              maxAge: 300000
+            }
+          }
+        }
+      }
     }
   });
 
@@ -126,4 +171,5 @@ module.exports = function (grunt) {
     'usemin'
   ]);
   grunt.registerTask('default', ['build']);
+  grunt.registerTask('serve',['build','connect:dist','watch']);
 };
