e5b9bc50eac16f93bc70318f375d02afb2cb8e5e
xthecapx <xthecapx@gmail.com>
2016-05-11 21:20:29 -0500

    Exercise 3.2.3 Added

--- 3-angularjs/week2/conFusionG/app/scripts/app.js
+++ 3-angularjs/week2/conFusionG/app/scripts/app.js
@@ -65,4 +65,41 @@ angular
     $scope.toggleDetails = function() {
       $scope.showDetails = !$scope.showDetails;
     };
+  }])
+  .controller('ContactController', ['$scope', function($scope) {
+    $scope.feedback = {
+      mychannel: "",
+      firstName: "",
+      lastName: "",
+      agree: false,
+      email: ""
+    };
+
+    $scope.channels = [
+      {value:"tel", label:"Tel."},
+      {value:"Email",label:"Email"}
+    ];
+
+    $scope.invalidChannelSelection = false;
+  }])
+  .controller('FeedbackController', ['$scope', function($scope) {
+    $scope.sendFeedback = function() {
+      console.log($scope.feedback);
+      if ($scope.feedback.agree && ($scope.feedback.mychannel == "") && !$scope.feedback.mychannel) {
+        $scope.invalidChannelSelection = true;
+        console.log('incorrect');
+      } else {
+        $scope.invalidChannelSelection = false;
+        $scope.feedback = {
+          mychannel: "",
+          firstName: "",
+          lastName: "",
+          agree: false,
+          email: ""
+        };
+        $scope.feedback.mychannel="";
+        $scope.feedbackForm.$setPristine();
+        console.log($scope.feedback);
+      }
+    };
   }]);
