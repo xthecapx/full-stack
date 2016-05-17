3012164f5ad0cd015bf04e071e0b07b665407088
Cristian Daniel Marquez <c.marquez@globant.com>
2016-05-17 13:05:51 -0500

    Clean week2 assigment

--- 3-angularjs/week2/assignment/app.js
+++ 3-angularjs/week2/assignment/app.js
@@ -2,7 +2,7 @@
 
 angular.module('confusionApp', [])
 
-        .controller('dishDetailController', ['$scope', function($scope) {
+        .controller('DishDetailController', ['$scope', function($scope) {
           $scope.dish = {
             name:'Uthapizza',
             image: '../conFusion/app/images/uthapizza.png',
@@ -48,87 +48,6 @@ angular.module('confusionApp', [])
             {id: '2', value: 'date', reverse: false},
             {id: '3', value: 'rating', reverse: false}
           ];
-        }])
-
-        .controller('ContactController', ['$scope', function($scope) {
-
-            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
-
-            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
-
-            $scope.channels = channels;
-            $scope.invalidChannelSelection = false;
-
-        }])
-
-        .controller('FeedbackController', ['$scope', function($scope) {
-
-            $scope.sendFeedback = function() {
-
-                console.log($scope.feedback);
-
-                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
-                    $scope.invalidChannelSelection = true;
-                    console.log('incorrect');
-                }
-                else {
-                    $scope.invalidChannelSelection = false;
-                    $scope.feedback = {
-                      name: "",
-                      rating: 5,
-                      comments: ""
-                    };
-                    $scope.feedback.mychannel="";
-                    $scope.feedbackForm.$setPristine();
-                    console.log($scope.feedback);
-                }
-            };
-            console.log($scope.feedback);
-        }])
-
-        .controller('DishDetailController', ['$scope', function($scope) {
-
-            $scope.dish = {
-                          name:'Uthapizza',
-                          image: 'images/uthapizza.png',
-                          category: 'mains',
-                          label:'Hot',
-                          price:'4.99',
-                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-                           comments: [
-                               {
-                                   rating:5,
-                                   comment:"Imagine all the eatables, living in conFusion!",
-                                   author:"John Lemon",
-                                   date:"2012-10-16T17:57:28.556094Z"
-                               },
-                               {
-                                   rating:4,
-                                   comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-                                   author:"Paul McVites",
-                                   date:"2014-09-05T17:57:28.556094Z"
-                               },
-                               {
-                                   rating:3,
-                                   comment:"Eat it, just eat it!",
-                                   author:"Michael Jaikishan",
-                                   date:"2015-02-13T17:57:28.556094Z"
-                               },
-                               {
-                                   rating:4,
-                                   comment:"Ultimate, Reaching for the stars!",
-                                   author:"Ringo Starry",
-                                   date:"2013-12-02T17:57:28.556094Z"
-                               },
-                               {
-                                   rating:2,
-                                   comment:"It's your birthday, we're gonna party!",
-                                   author:"25 Cent",
-                                   date:"2011-12-02T17:57:28.556094Z"
-                               }
-
-                           ]
-                    };
         }])
 
         .controller('DishCommentController', ['$scope', function($scope) {
