f46318c8d2267d2d670f6e8bf6e3dd68e3bc45ec
Cristian Daniel Marquez <c.marquez@globant.com>
2016-05-19 09:34:45 -0500

    Exercise 3.3.1 Removed feedbackcontroller

--- 3-angularjs/week3/conFusion/scripts/controllers.js
+++ 3-angularjs/week3/conFusion/scripts/controllers.js
@@ -31,28 +31,6 @@ angular
     };
   }])
 
-  .controller('FeedbackController', ['$scope', function($scope) {
-    $scope.sendFeedback = function() {
-      console.log($scope.feedback);
-      if ($scope.feedback.agree && ($scope.feedback.mychannel == "") && !$scope.feedback.mychannel) {
-        $scope.invalidChannelSelection = true;
-        console.log('incorrect');
-      } else {
-        $scope.invalidChannelSelection = false;
-        $scope.feedback = {
-          mychannel: "",
-          firstName: "",
-          lastName: "",
-          agree: false,
-          email: ""
-        };
-        $scope.feedback.mychannel="";
-        $scope.feedbackForm.$setPristine();
-        console.log($scope.feedback);
-      }
-    };
-  }])
-
   .controller('ContactController', ['$scope', function($scope) {
 
       $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
