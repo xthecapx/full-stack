152c795e2c443b004b23746a366cf478f9ec12bc
Cristian Daniel Marquez <c.marquez@globant.com>
2016-05-19 13:23:35 -0500

    Excersice 3.3.2: Templates Added

--- 3-angularjs/week3/conFusion/scripts/controllers.js
+++ 3-angularjs/week3/conFusion/scripts/controllers.js
@@ -77,4 +77,41 @@ angular
             date: ""
           };
       };
+  }])
+
+  .controller('ContactController', ['$scope', function($scope) {
+    $scope.feedback = {
+      mychannel: "",
+      firstName: "",
+      lastName: "",
+      agree: false,
+      email: ""
+    };
+
+    $scope.channels = [
+      {value:"tel", label:"Tel."},
+      {value:"Email",label:"Email"}
+    ];
+
+    $scope.invalidChannelSelection = false;
+  }])
+
+  .controller('FeedbackController', ['$scope', function($scope) {
+    $scope.sendFeedback = function() {
+      if ($scope.feedback.agree && ($scope.feedback.mychannel == "") && !$scope.feedback.mychannel) {
+        $scope.invalidChannelSelection = true;
+        console.log('incorrect');
+      } else {
+        $scope.invalidChannelSelection = false;
+        $scope.feedback = {
+          mychannel: "",
+          firstName: "",
+          lastName: "",
+          agree: false,
+          email: ""
+        };
+        $scope.feedback.mychannel="";
+        $scope.feedbackForm.$setPristine();
+      }
+    };
   }]);
